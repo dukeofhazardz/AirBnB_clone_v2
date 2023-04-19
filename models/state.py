5efc8de03f9bd778d026bf9ea9fa10f68768495b
Daniel John <dj71286@gmail.com>
2023-04-19 17:40:46 +0100

    Updated model files

--- models/state.py
+++ models/state.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from os import getenv
+import models
 from models.base_model import BaseModel
 from models.base_model import Base
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
