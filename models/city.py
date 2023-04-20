6995e02e63117304ae999a8a506f6d0c78d82a6a
Daniel John <dj71286@gmail.com>
2023-04-20 09:12:29 +0100

    testing1

--- models/city.py
+++ models/city.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-<<<<<<< HEAD
 from models.base_model import Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-=======
->>>>>>> parent of 95603be (Updated and improved do_created and set up datatbases)
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-<<<<<<< HEAD
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     places = relationship("Place", backref="cities", cascade="delete")
-=======
-    state_id = ""
-    name = ""
->>>>>>> parent of 95603be (Updated and improved do_created and set up datatbases)
--- models/state.py
+++ models/state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-<<<<<<< HEAD
 from os import getenv
 import models
 from models.base_model import BaseModel
@@ -8,10 +7,6 @@ from models.base_model import Base
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-=======
-from models.base_model import BaseModel
->>>>>>> parent of 95603be (Updated and improved do_created and set up datatbases)
-
 
 class State(BaseModel):
     """ State class """
