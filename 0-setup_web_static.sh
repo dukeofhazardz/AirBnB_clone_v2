e06fad6e42a1ed166f1e1b6c6c6d113160620251
Daniel John <dj71286@gmail.com>
2023-05-04 23:57:47 +0100

    fix at setUp webweb

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
-# A bash script that sets up a web server for deployment of web_static.
-
+#sets up on a server
 sudo apt-get -y update
-sudo apt-get -y nginx
-
+sudo apt-get -y install nginx
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/current/
-sudo echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current/
+sudo echo -e "<html>
+<head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-serve="location /hbnb_static/{\n\talias /data/web_static/current/;\n}\n"
-sudo sed -i "27i $serve" /etc/nginx/sites-available/default
+file=/etc/nginx/sites-available/default
+phrase="location /hbnb_static/{\n\talias /data/web_static/current/;\n}\n"
+sudo sed -i "27i $phrase" $file
 sudo service nginx restart
