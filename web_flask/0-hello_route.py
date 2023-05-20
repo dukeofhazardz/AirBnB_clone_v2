7f73c113026f45ce6dbb4edfbf4bae5002776a6d
Daniel John <dj71286@gmail.com>
2023-05-20 20:06:51 +0100

    fix at 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-# A script that starts a Flask web application
+""" A script that starts a Flask web application """
 
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/', strict_slashes=False)
@@ -13,4 +14,4 @@ def hello_hbnb():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0')
